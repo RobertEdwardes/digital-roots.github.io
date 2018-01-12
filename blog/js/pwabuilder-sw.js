f1a6270120b5f0b530eef5e6d52cdc30c2cf0336
Robert Edwardes <rdedwardes@gmail.com>
2018-01-11 20:00:52 -0500

    fixed PWA

--- blog/js/pwabuilder-sw.js
+++ blog/js/pwabuilder-sw.js
@@ -1,57 +1,52 @@
-var CACHE = 'pwabuilder-precache';
-var precacheFiles = [
-
-    ];
-
-self.addEventListener('install', function(evt) {
-  console.log('The service worker is being installed.');
-  evt.waitUntil(precache().then(function() {
-    console.log('[ServiceWorker] Skip waiting on install');
-      return self.skipWaiting();
-
-  })
-  );
+self.addEventListener('install', function(event) {
+  event.waitUntil(preLoad());
 });
 
-
-//allow sw to control of current page
-self.addEventListener('activate', function(event) {
-console.log('[ServiceWorker] Claiming clients for current page');
-      return self.clients.claim();
-
-});
-
-self.addEventListener('fetch', function(evt) {
-  console.log('The service worker is serving the asset.'+ evt.request.url);
-  evt.respondWith(fromCache(evt.request).catch(fromServer(evt.request)));
-  evt.waitUntil(update(evt.request));
-});
-
-
-function precache() {
-  return caches.open(CACHE).then(function (cache) {
-    return cache.addAll(precacheFiles);
+var preLoad = function(){
+  console.log('[PWA Builder] Install Event processing');
+  return caches.open('pwabuilder-offline').then(function(cache) {
+    console.log('[PWA Builder] Cached index and offline page during Install');
+    return cache.addAll(['/offline.html', '/index.html']);
   });
 }
 
+self.addEventListener('fetch', function(event) {
+  console.log('The service worker is serving the asset.');
+  event.respondWith(checkResponse(event.request).catch(function() {
+    return returnFromCache(event.request)}
+  ));
+  event.waitUntil(addToCache(event.request));
+});
 
-function fromCache(request) {
-  return caches.open(CACHE).then(function (cache) {
-    return cache.match(request).then(function (matching) {
-      return matching || Promise.reject('no-match');
-    });
+var checkResponse = function(request){
+  return new Promise(function(fulfill, reject) {
+    fetch(request).then(function(response){
+      if(response.status !== 404) {
+        fulfill(response)
+      } else {
+        reject()
+      }
+    }, reject)
   });
-}
+};
 
-
-function update(request) {
-  return caches.open(CACHE).then(function (cache) {
+var addToCache = function(request){
+  return caches.open('pwabuilder-offline').then(function (cache) {
     return fetch(request).then(function (response) {
+      console.log('[PWA Builder] add page to offline'+response.url)
       return cache.put(request, response);
     });
   });
-}
+};
 
-function fromServer(request){
-return fetch(request).then(function(response){ return response})
-}
+var returnFromCache = function(request){
+  return caches.open('pwabuilder-offline').then(function (cache) {
+    return cache.match(request).then(function (matching) {
+     if(!matching || matching.status == 404) {
+       return cache.match('offline.html')
+     } else {
+       return matching
+     }
+    });
+  });
+};
